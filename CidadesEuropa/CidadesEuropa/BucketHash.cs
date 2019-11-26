f48744629ea70c6fb6a166eb62b1d0e219604f83
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-11-26 16:17:28 -0300

    bucket hash, spinner e começo grafo
    passamos a lista de cidades para bucket hash, mostramos as cidades nos spinners de origem e destino e começamos a trabalhar com o grafo de djikstra

---
+++ CidadesEuropa/CidadesEuropa/BucketHash.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class BucketHash
+{
+    private const int SIZE = 103;
+    ListaSimples<Cidade>[] data;
+    public BucketHash()
+    {
+        data = new ListaSimples<Cidade>[SIZE];
+        for (int i = 0; i < SIZE; i++)
+            data[i] = new ListaSimples<Cidade>();
+    }
+
+    public int Hash(string s)
+    {
+        long tot = 0;
+        char[] charray;
+        charray = s.ToUpper().ToCharArray();
+        for (int i = 0; i <= s.Length - 1; i++)
+            tot += 37 * tot + (int)charray[i];
+        tot = tot % data.GetUpperBound(0);
+        if (tot < 0)
+            tot += data.GetUpperBound(0);
+        return (int)tot;
+    }
+
+    public void Insert(Cidade c)
+    {
+        int hash_value = Hash(c.NomeCidade);
+        if (!data[hash_value].ExisteDado(c))
+            data[hash_value].InserirAposFim(c);
+    }
+    public bool Remove(Cidade c)
+    {
+        int hash_value = Hash(c.NomeCidade);
+        if (data[hash_value].ExisteDado(c))
+        {
+            data[hash_value].Remover(c);
+            return true;
+        }
+        return false;
+    }
+
+    public ListaSimples<Cidade> getPosicao(int hash)
+    {
+        return data[hash];
+    }
+}
--- CidadesEuropa/CidadesEuropa/MainActivity.cs
+++ CidadesEuropa/CidadesEuropa/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.OS;
 using System.IO;
 using System;
 using Android.Content.Res;
+using System.Collections;
 
 namespace CidadesEuropa
 {
@@ -11,7 +12,10 @@ namespace CidadesEuropa
     public class MainActivity : Activity
     {
         Button btnBuscar;
-        ListaSimples<Cidade> listaCidades = new ListaSimples<Cidade>();
+        Spinner sOrigem, sDestino;
+
+        BucketHash listaCidades = new BucketHash();
+        InformacoesPercurso[,] adjacencias;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -19,18 +23,71 @@ namespace CidadesEuropa
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            
+
+            btnBuscar = FindViewById<Button>(Resource.Id.btnBuscar);
+            sOrigem = FindViewById<Spinner>(Resource.Id.spinnerOrigem);
+            sDestino = FindViewById<Spinner>(Resource.Id.spinnerDestino);
+
             AssetManager assets = this.Assets;
 
+            IList listaNomes = new ArrayList();
+            int quantasCidades = 0;
             using (StreamReader leitor = new StreamReader(assets.Open("Cidades.txt")))
             {
                 while (!leitor.EndOfStream)
                 {
-                    leitor.ReadLine();
                     Cidade cidade = Cidade.LerArquivo(leitor);
-                    listaCidades.InserirAposFim(cidade);
+                    listaCidades.Insert(cidade);
+                    quantasCidades++;
+                    listaNomes.Add(cidade.NomeCidade);
                 }
             }
+
+            sOrigem.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, listaNomes);
+            sDestino.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, listaNomes);
+
+            adjacencias = new InformacoesPercurso[quantasCidades,quantasCidades];
+
+            MontarMatriz();
+        }
+
+        private void MontarMatriz()
+        {
+            using (StreamReader leitor = new StreamReader(Assets.Open("GrafoTremEspanhaPortugal.txt")))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    string linha = leitor.ReadLine();
+                    string nomeCidadeOrigem = linha.Substring(0, 15).Trim();
+                    string nomeCidadeDestino = linha.Substring(15, 15).Trim();
+                    int distancia = int.Parse(linha.Substring(30, 5).Trim());
+                    int tempo = int.Parse(linha.Substring(35, 3).Trim());
+                    int idCidadeOrigem = ProcurarCidadePorNome(nomeCidadeOrigem);
+                    int idCidadeDestino = ProcurarCidadePorNome(nomeCidadeDestino);
+                    adjacencias[idCidadeOrigem, idCidadeDestino] = new InformacoesPercurso(distancia, tempo);
+                }
+            }
+        }
+
+        private int ProcurarCidadePorNome(string nome)
+        {
+            ListaSimples<Cidade> lista = listaCidades.getPosicao(listaCidades.Hash(nome));
+
+            NoLista<Cidade> atual = lista.Primeiro;
+
+            while (atual != null)
+            {
+                if (atual.Info.NomeCidade.Equals(nome))
+                    return atual.Info.IdCidade;
+                else
+                    atual = atual.Prox;
+            }
+            return -1;
+        }
+
+        private void MostrarCidadesNaView()
+        {
+
         }
     }
 }
