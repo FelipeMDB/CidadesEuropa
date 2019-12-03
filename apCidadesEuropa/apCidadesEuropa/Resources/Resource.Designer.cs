27a3ba17900e90fea8cd6037b54d2a3ee4864f23
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-12-03 11:19:14 -0300

    não fiz nada e sumiu o botao

--- apCidadesEuropa/apCidadesEuropa/Resources/Resource.Designer.cs
+++ apCidadesEuropa/apCidadesEuropa/Resources/Resource.Designer.cs
@@ -60,17 +60,17 @@ namespace apCidadesEuropa
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050015
-			public const int btnAddCaminho = 2131034133;
+			// aapt resource value: 0x7f050013
+			public const int btnAddCaminho = 2131034131;
 			
-			// aapt resource value: 0x7f050014
-			public const int btnAddCidade = 2131034132;
+			// aapt resource value: 0x7f050012
+			public const int btnAddCidade = 2131034130;
 			
 			// aapt resource value: 0x7f050009
 			public const int btnAdicionarCidade = 2131034121;
 			
-			// aapt resource value: 0x7f050011
-			public const int btnBuscar = 2131034129;
+			// aapt resource value: 0x7f05000e
+			public const int btnBuscar = 2131034126;
 			
 			// aapt resource value: 0x7f050005
 			public const int btnInserirCaminho = 2131034117;
@@ -90,20 +90,14 @@ namespace apCidadesEuropa
 			// aapt resource value: 0x7f050008
 			public const int edtYCidade = 2131034120;
 			
-			// aapt resource value: 0x7f05000a
-			public const int linearLayout1 = 2131034122;
-			
-			// aapt resource value: 0x7f05000b
-			public const int linearLayout2 = 2131034123;
-			
-			// aapt resource value: 0x7f05000e
-			public const int linearLayout3 = 2131034126;
-			
-			// aapt resource value: 0x7f050010
-			public const int spinnerDestino = 2131034128;
+			// aapt resource value: 0x7f05000f
+			public const int imgMapa = 2131034127;
 			
 			// aapt resource value: 0x7f05000d
-			public const int spinnerOrigem = 2131034125;
+			public const int spinnerDestino = 2131034125;
+			
+			// aapt resource value: 0x7f05000b
+			public const int spinnerOrigem = 2131034123;
 			
 			// aapt resource value: 0x7f050001
 			public const int spnNovaOrigem = 2131034113;
@@ -111,20 +105,20 @@ namespace apCidadesEuropa
 			// aapt resource value: 0x7f050002
 			public const int spnNovoDestino = 2131034114;
 			
+			// aapt resource value: 0x7f05000a
+			public const int textView1 = 2131034122;
+			
 			// aapt resource value: 0x7f05000c
-			public const int textView1 = 2131034124;
+			public const int textView2 = 2131034124;
 			
-			// aapt resource value: 0x7f05000f
-			public const int textView2 = 2131034127;
-			
-			// aapt resource value: 0x7f050012
-			public const int textView3 = 2131034130;
+			// aapt resource value: 0x7f050010
+			public const int textView3 = 2131034128;
 			
 			// aapt resource value: 0x7f050000
 			public const int textView7 = 2131034112;
 			
-			// aapt resource value: 0x7f050013
-			public const int tvResultado = 2131034131;
+			// aapt resource value: 0x7f050011
+			public const int tvResultado = 2131034129;
 			
 			static Id()
 			{
